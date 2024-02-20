74219ced5429a0e70199630476a894a86c7e1852
Sebastiano <sebastianolaini@gmail.com>
2024-02-20 22:18:41 +0100

    single quote

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "extends": ["eslint-config-next", "plugin:prettier/recommended", "plugin:tailwindcss/recommended", "prettier"],
-  "plugins": ["tailwindcss"],
-  "rules": {
+  'extends': ['eslint-config-next', 'plugin:prettier/recommended', 'plugin:tailwindcss/recommended', 'prettier'],
+  'plugins': ['tailwindcss'],
+  'rules': {
     'no-console': 'warn',
-    "no-var": "error",
+    'no-var': 'error',
     'no-unused-vars': 'warn',
-    "prefer-const": [
-      "error", {
-        "destructuring": "any",
-        "ignoreReadBeforeAssign": false
+    'prefer-const': [
+      'error', {
+        'destructuring': 'any',
+        'ignoreReadBeforeAssign': false
       }
     ],
     camelcase: [
@@ -18,7 +18,7 @@ module.exports = {
       },
     ],
     yoda: ['error', 'never'],
-    "tailwindcss/no-custom-classname": 0,
+    'tailwindcss/no-custom-classname': 0,
   },
   env: {
     es2021: true,
--- prettierrc.js
+++ prettierrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "parser": "flow",
-  "printWidth": 10000,
-  "endOfLine": "lf",
-  "trailingComma": "es5",
-  "tabWidth": 2,
-  "semi": false,
-  "singleQuote": true,
-  "jsxSingleQuote": true,
-  "singleAttributePerLine": true,
-  "bracketSpacing": true,
-  "bracketSameLine": false
+  'parser': 'flow',
+  'printWidth': 10000,
+  'endOfLine': 'lf',
+  'trailingComma': 'es5',
+  'tabWidth': 2,
+  'semi': false,
+  'singleQuote': true,
+  'jsxSingleQuote': true,
+  'singleAttributePerLine': true,
+  'bracketSpacing': true,
+  'bracketSameLine': false
 }
