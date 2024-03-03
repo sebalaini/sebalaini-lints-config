e33e723bf35e95423cd7eb16a0a9572e8fdbf27e
Sebastiano <sebastianolaini@gmail.com>
2024-03-03 14:23:23 +0100

    add tailwind ignore rules

--- stylelintrc.js
+++ stylelintrc.js
@@ -4,5 +4,17 @@ module.exports = {
     'media-feature-range-notation': 'prefix',
     'color-function-notation': 'legacy',
     'font-family-no-missing-generic-family-keyword': null,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          // 'variants',
+          // 'responsive',
+          // 'screen',
+        ],
+      },
+    ],
   },
 }
